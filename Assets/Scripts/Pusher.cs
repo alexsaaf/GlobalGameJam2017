91fc4a5c5057dcb483c5b71ae2cd8b27a7a1f0f7
Alexander Sääf <Alexandersaaf95@gmail.com>
2017-01-20 23:56:50 +0100

    Add pushers and handling of pushers

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Pusher : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Color activatedColor;
+    public float activatedTime;
+
+    public string identity;
+
+    Color standardColor;
+
+    SpriteRenderer sr;
+
+    void Awake() {
+        identity = new string(identity.OrderBy(c => c).ToArray()).ToUpper();
+    }
+
+    void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        standardColor = sr.color;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ActivateIdentity(string idToActivate, Color playerColor) {
+
+        if(idToActivate.Equals(identity)) {
+            Debug.Log("Activating");
+            activatedColor = playerColor;
+            Activate();
+        }
+    }
+
+    public void Activate() {
+        StartCoroutine("ActivateIndication");
+    }
+
+    IEnumerator ActivateIndication() {
+        sr.color = activatedColor;
+        yield return new WaitForSeconds(activatedTime);
+        sr.color = standardColor;
+    }
 }
---
+++ Assets/Scripts/PusherTesting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PusherTesting : MonoBehaviour {
+
+    public Color green;
+    public Color red;
+
+    public InputField inputText;
+
+    void Start() {
+        inputText = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    public void ActivateRed() {
+        PusherHandler.instance.ActivatePusher(inputText.text, red);
+        inputText.text = "";
+    }
+
+    public void ActivateGreen() {
+        PusherHandler.instance.ActivatePusher(inputText.text, green);
+        inputText.text = "";
+    }
+}
