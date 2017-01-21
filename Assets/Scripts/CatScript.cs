0d053fbcb81ddec896fade4b92d006b596e44abd
Erik Mansén <erima668@student.liu.se>
2017-01-21 17:36:37 +0100

    Hopefully add sound to the cats

--- Assets/Scripts/CatScript.cs
+++ Assets/Scripts/CatScript.cs
@@ -6,9 +6,25 @@ public class CatScript : MonoBehaviour {
 
     public float score;
     public int playerNumber;
+    public AudioClip meow1;
+    public AudioClip meow2;
+    public int minMeowCooldown;
+    public int maxMeowCooldown;
+    private float meowTimer = 0;
 	
 	// Update is called once per frame
 	void Update () {
+        if(meowTimer <= 0) {
+            int meowPicker = Random.Range(0, 2);
+            if(meowPicker == 0) {
+                AudioSource.PlayClipAtPoint(meow1, transform.position);
+            } else {
+                AudioSource.PlayClipAtPoint(meow2, transform.position);
+            }
+            meowTimer += Random.Range(minMeowCooldown, maxMeowCooldown);
+        } else {
+            meowTimer -= Time.deltaTime;
+        }
 	}
 
     public void OnDeath () {
