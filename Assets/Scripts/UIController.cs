5472813a91ac62573273caa9113aca7d92260427
Alexander Sääf <Alexandersaaf95@gmail.com>
2017-01-21 17:01:42 +0100

    Add changes to UI

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -69,7 +69,7 @@ public class UIController : MonoBehaviour {
         }
 
         public void AssignSymbol(Text symbol, char c) {
-            string character = c.ToString().ToUpper();
+            string character = c.ToString()§.ToUpper();
             symbol.text = character;
             switch (character) {
                 case "E":
