32def8fa6ceca92c37f7ccee50ebc8307898488f
Philip Johansson <philip_yeah@hotmail.com>
2017-01-21 13:08:24 +0100

    Add fading between scenes and scene loading.

---
+++ Assets/Scripts/GameManagerStuff/GameManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+
+    // The score for the players
+    public float playerOneScore = 0;
+    public float playerTwoScore = 0;
+
+    public float winScore = 10f;
+
+    private SceneHandler sceneHandler;
+
+	// Use this for initialization
+	void Start () {
+        sceneHandler = GetComponent<SceneHandler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerOneScore >= winScore || playerTwoScore >= winScore) {
+            sceneHandler.LoadEndScene();
+        }
+        if (Input.GetButtonDown("EndGame")) {
+            sceneHandler.LoadMainMenu();
+        }
+        if (Input.GetButtonDown("Next")) {
+            Debug.Log("Loading the NExt scene");
+            sceneHandler.LoadNextScene();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManagerStuff/SceneHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour {
+
+    private Fading fading;
+
+	// Use this for initialization
+	void Start () {
+        SceneHandler.DontDestroyOnLoad(gameObject);
+        fading = GetComponent<Fading>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadMainMenu() {
+        StartCoroutine("MainSceneLoadFading");
+    }
+
+    public void LoadEndScene() {
+        StartCoroutine("EndSceneLoadingFading");
+    }
+
+    public void LoadNextScene() {
+        Debug.Log("FUCK YOU I AM A SCENELOADER");
+        StartCoroutine("NextScene");
+    }
+
+    IEnumerator MainSceneLoadFading() {
+        float fadeTime = fading.BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    IEnumerator EndSceneLoadFading() {
+        float fadeTime = fading.BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadSceneAsync("EndScene");
+    }
+
+    IEnumerator NextScene() {
+        float fadeTime = fading.BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
