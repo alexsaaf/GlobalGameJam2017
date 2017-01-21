2ec73243a57f598072cb4f01d8483036f19ea2df
Erik Mansén <erima668@student.liu.se>
2017-01-21 12:18:49 +0100

    Add Sawblade prefab, SawBlade tag and a very rudimentary Main Menu

---
+++ Assets/Scripts/SawBladeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBladeScript : MonoBehaviour {
+
+    public float speed;
+    private Transform sawBlade;
+
+    void Start() {
+        sawBlade = (Transform)GetComponent(typeof(Transform));
+    }
+
+    void FixedUpdate () {
+        sawBlade.Rotate(new Vector3(0, 0, speed * Time.fixedDeltaTime));
+    }
+}
