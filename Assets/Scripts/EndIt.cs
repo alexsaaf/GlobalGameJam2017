e81ba3e354e37ca65648efd41c020e065e3df1fc
Philip Johansson <philip_yeah@hotmail.com>
2017-01-22 12:20:07 +0100

    fix som bugs and an end screen.

---
+++ Assets/Scripts/EndIt.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndIt : MonoBehaviour {
+
+	public void TheEnd() {
+        Application.Quit();
+    }
+}
