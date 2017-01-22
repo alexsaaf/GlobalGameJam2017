b9f5877ff68245d165ede52529d0ab5c01c48cef
Philip Johansson <philip_yeah@hotmail.com>
2017-01-22 12:57:42 +0100

    end scene fix.

--- Assets/Scripts/EndIt.cs
+++ Assets/Scripts/EndIt.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class EndIt : MonoBehaviour {
 
+    SceneHandler SH;
+
+    void Start() {
+        SH = GameObject.Find("GameManager").GetComponent<SceneHandler>();
+    }
+
+    public void MainMenu() {
+        SH.LoadMainMenu();
+    }
+
 	public void TheEnd() {
         Application.Quit();
     }
